b39f71debcaf144dd0ac8f71d3a94fe94f814847
Karthik P <kprasan@hotmail.com>
2018-05-28 18:54:46 -0700

    error handling; cleanups

--- schedule_classes.py
+++ schedule_classes.py
@@ -6,39 +6,49 @@ class Node:
     def __init__(self, name, prereq_names):
         self.name = name
         self.prereq_names = prereq_names
-        self.links = []
+        self.prereq_links = []
         self.tier = None
 
 
 def parse_input(input_file):
     # read file
-    f = open(input_file, "r")
-    i = f.read()
-    f.close()
-    print i
-    entries = json.loads(i)
+    entries = None
+    try:
+        file_handle = open(input_file, "r")
+        file_content = file_handle.read()
+        file_handle.close()
+    except Exception as e:
+        print e
+        raise Exception ("Unable to read file")
+    else:
+        print file_content
+        entries = json.loads(file_content)
+    if not entries:
+        raise Exception("No valid entries found in input file")
     return entries
 
 
-def create_nodes_dict(entries):
+def create_and_link_graph(entries):
+    # create dict
     nodes = {}
     for entry in entries:
         nodes[entry['name']] = Node(entry['name'], entry['prerequisites'])
-    return nodes
 
-
-def link_graph(nodes):
+    # link prereqs
     for entry in nodes:
         node = nodes[entry]
         for prereq in node.prereq_names:
-            node.links.append(nodes[prereq])
+            if not prereq in nodes:
+                raise Exception("Unable to find prequisite %s in input" % prereq)
+            node.prereq_links.append(nodes[prereq])
 
+    return nodes
 
 def find_node_tier_if_ready(node):
     max_prereq_tier = -1 # 0 will be returned for nodes with no prereqs
     missing = None
 
-    for prereq in node.links:
+    for prereq in node.prereq_links:
         if prereq.tier is None:
             # prereq node has not been processed yet
             missing = prereq
@@ -93,8 +103,7 @@ def main(args):
 
     # create and Link the nodes
     print "\n--->Building the graph"
-    nodes = create_nodes_dict(entries)
-    link_graph(nodes)
+    nodes = create_and_link_graph(entries)
 
     # Organize graph by tiers (with DFS)
     print "\n--->Tiering the graph - to support BFS walking"
